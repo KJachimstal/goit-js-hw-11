b00151d77de4c5eec03beb3532623d8fc11b0a42
Konrad Jachimstal <k.jachimstal@gmail.com>
2023-03-24 20:33:42 +0100

    Added simplelightbox

--- src/index.js
+++ src/index.js
@@ -1,4 +1,6 @@
 import axios from 'axios';
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
 
 // const searchBox = document.querySelector('#search-form');
 // const searchButton = document.querySelector('.search-button');
@@ -7,6 +9,7 @@ const results = document.querySelector('.results');
 const axios = require('axios');
 
 searchForm.addEventListener('submit', searchImages);
+// results.addEventListener('click', onImageClick);
 
 function searchImages(event) {
   event.preventDefault();
@@ -27,6 +30,9 @@ function searchImages(event) {
       let data = JSON.parse(response.request.response).hits;
       //   console.log(data.length);
       renderImages(data);
+      new SimpleLightbox('.results a', {
+        captionsData: 'alt',
+      });
     })
     .catch(function (error) {
       console.log(error);
@@ -36,26 +42,41 @@ function searchImages(event) {
 
 function renderImages(data) {
   const markup = data
-    .map(({ webformatURL, tags, likes, views, comments, downloads }) => {
-      return `<div class="photo-card">
-    <img src="${webformatURL}" alt="${tags}" loading="lazy" />
-    <div class="info">
-      <p class="info-item">
-        <b>Likes: </b></br>${likes}
-      </p>
-      <p class="info-item">
-        <b>Views: </b></br>${views}
-      </p>
-      <p class="info-item">
-        <b>Comments: </b></br>${comments}
-      </p>
-      <p class="info-item">
-        <b>Downloads: </b></br>${downloads}
-      </p>
-    </div>
-  </div>
+    .map(
+      ({
+        webformatURL,
+        largeImageURL,
+        tags,
+        likes,
+        views,
+        comments,
+        downloads,
+      }) => {
+        return `
+      <div class="photo-card">
+        <div class="gallery">
+        <a href="${largeImageURL}">
+            <img src="${webformatURL}" alt="${tags}" loading="lazy" />
+        </a>
+        </div>
+        <div class="info">
+        <p class="info-item">
+            <b>Likes: </b></br>${likes}
+        </p>
+        <p class="info-item">
+            <b>Views: </b></br>${views}
+        </p>
+        <p class="info-item">
+            <b>Comments: </b></br>${comments}
+        </p>
+        <p class="info-item">
+            <b>Downloads: </b></br>${downloads}
+        </p>
+        </div>
+      </div>
     `;
-    })
+      }
+    )
     .join('');
   results.innerHTML = markup;
 }
